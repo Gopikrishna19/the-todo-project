b0619d85cacee8cdf5a68c3476cb823cc5250fb7
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-05-26 22:58:11 -0500

    Improve error handling and logging

--- src/server/utils/api-error-trip.js
+++ src/server/utils/api-error-trip.js
@@ -1,8 +1,9 @@
 const {send, statuses} = require('./responses');
+const {log} = require('../../utils/logger');
 
 module.exports.trip = (req, res, handler) => (error, ...args) => {
     if (error) {
-        console.log(error); // eslint-disable-line
+        log('Handling', req.originalUrl, 'failed:', error.message);
 
         return send(statuses.INTERNAL_SERVER_ERROR)(req, res);
     }
