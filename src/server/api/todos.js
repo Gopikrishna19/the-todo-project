abde7a73ef2edcf0009333761541461f8f934206
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-05-26 22:58:00 -0500

    Handling fetching single todo

--- src/server/api/todos.js
+++ src/server/api/todos.js
@@ -5,8 +5,24 @@ const {trip} = require('../utils/api-error-trip');
 
 const router = new express.Router();
 
-router.get('/', (req, res) => Todo.find().exec(trip(req, res, todos => res.send({todos}))));
+router.get(
+    '/',
+    (req, res) => Todo
+        .find()
+        .exec(trip(req, res, todos => res.send({todos})))
+);
 
-router.post('/', (req, res) => new Todo(req.body).save(trip(req, res, ({_id: id}) => res.send({id}))));
+router.post(
+    '/',
+    (req, res) => new Todo(req.body)
+        .save(trip(req, res, ({_id: id}) => res.send({id})))
+);
+
+router.get(
+    '/:id',
+    (req, res) => Todo
+        .find({_id: req.params.id})
+        .exec(trip(req, res, todos => res.send({todos})))
+);
 
 module.exports = router;
